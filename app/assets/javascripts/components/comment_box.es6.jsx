6fe62d2098702192699c6f793e2a4cd988395153
blueplanet <erguolinge@gmail.com>
2016-12-14 06:43:44 +0900

    Add comment box

---
+++ app/assets/javascripts/components/comment_box.es6.jsx
@@ -0,0 +1,13 @@
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='commentBox'>
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+}
