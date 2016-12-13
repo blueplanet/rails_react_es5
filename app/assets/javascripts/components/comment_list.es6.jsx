156effd9da4820a474a6d669798ce14e58616f61
blueplanet <erguolinge@gmail.com>
2016-12-14 06:52:53 +0900

    add comment

--- app/assets/javascripts/components/comment_list.es6.jsx
+++ app/assets/javascripts/components/comment_list.es6.jsx
@@ -2,7 +2,8 @@ class CommentList extends React.Component {
   render() {
     return(
       <div className='commentList'>
-        CommentList
+        <Comment author='魚'>React良いよ。</Comment>
+        <Comment author='呉'>なるほど！！</Comment>
       </div>
     );
   }
