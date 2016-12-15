dc3671f717305c6d6ab19458369d715e4035ce39
blueplanet <erguolinge@gmail.com>
2016-12-15 09:46:54 +0900

    get comment form server

--- app/assets/javascripts/components/comment_box.es6.jsx
+++ app/assets/javascripts/components/comment_box.es6.jsx
@@ -1,6 +1,21 @@
 class CommentBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url: '/comments',
+      dataType: 'json',
+      cache: false,
+      success: (data) => { this.setState({data: data}); },
+      error: (xhr, status, error) => {
+        console.error(this.props.url, status, error.toString());
+      }
+    })
   }
 
   render() {
